c7d97a9674141ccf32d1bcf0924b69b6debd07f6
nhtc <cuong.mdc.99@gmail.com>
2025-09-15 00:11:26 +0700

    fix: show all member on payment page

--- src/app/payment/PaymentPageContent.tsx
+++ src/app/payment/PaymentPageContent.tsx
@@ -62,7 +62,7 @@ const PaymentPageContent = () => {
     const fetchMembers = async () => {
       try {
         setLoading(true)
-        const response = await fetch("/api/members?activeOnly=true") // Only fetch active members
+        const response = await fetch("/api/members") // Only fetch active members
         if (!response.ok) {
           throw new Error("Failed to fetch members")
         }
